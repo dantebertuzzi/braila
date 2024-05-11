6e14e209723921c095d3b2f0b94aeff750b8b8d3
Dante Bertuzzi <67659136+dantebertuzzi@users.noreply.github.com>
2024-05-11 16:19:34 -0300

    Update script.js
    mudança de logica

--- script.js
+++ script.js
@@ -1,134 +1,135 @@
-let timer;
-let studyTime = 30; // Tempo de estudo padrão em minutos
-let breakTime = 10;  // Tempo de pausa padrão em minutos
-let minutes = studyTime;
-let seconds = 0;
-let isRunning = false;
-let isBreak = false;
-let cyclesCompleted = 0;
+class BrailaTimer {
+    constructor() {
+        this.timer = null;
+        this.studyTime = 30; // Tempo de estudo padrão em minutos
+        this.breakTime = 10;  // Tempo de pausa padrão em minutos
+        this.minutes = this.studyTime;
+        this.seconds = 0;
+        this.isRunning = false;
+        this.isBreak = false;
+        this.cyclesCompleted = 0;
 
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const resetButton = document.getElementById('reset');
-const timerDisplay = document.querySelector('.time-display');
-const studyTimeInput = document.getElementById('study-time');
-const breakTimeInput = document.getElementById('break-time');
-const studyValue = document.getElementById('study-value');
-const breakValue = document.getElementById('break-value');
-const cyclesDisplay = document.getElementById('cycles');
+        this.startButton = document.getElementById('start');
+        this.stopButton = document.getElementById('stop');
+        this.resetButton = document.getElementById('reset');
+        this.timerDisplay = document.querySelector('.time-display');
+        this.studyTimeInput = document.getElementById('study-time');
+        this.breakTimeInput = document.getElementById('break-time');
+        this.studyValue = document.getElementById('study-value');
+        this.breakValue = document.getElementById('break-value');
+        this.cyclesDisplay = document.getElementById('cycles');
 
-// Função para atualizar o temporizador
-function updateTimer() {
-    if (minutes === 0 && seconds === 0) {
-        if (isBreak) {
-            // Se for uma pausa, reinicia o temporizador de estudo
-            minutes = studyTime;
-            isBreak = false;
-            document.body.classList.remove('finished-background'); // Remove a classe de fundo laranja
+        this.startButton.addEventListener('click', () => this.toggleTimer());
+        this.stopButton.addEventListener('click', () => this.stopTimer());
+        this.resetButton.addEventListener('click', () => this.resetTimer());
+        this.studyTimeInput.addEventListener('input', () => this.updateSettings());
+        this.breakTimeInput.addEventListener('input', () => this.updateSettings());
+
+        this.soundPlayed = false; // Adiciona uma flag para controlar se o som já foi reproduzido
+
+        this.updateTimerDisplay();
+    }
+
+    toggleTimer() {
+        if (!this.isRunning) {
+            this.startTimer();
         } else {
-            // Se for um ciclo de estudo completo, inicia a pausa
-            minutes = breakTime;
-            isBreak = true;
-            cyclesCompleted++;
-            cyclesDisplay.textContent = `Ciclos completos: ${cyclesCompleted}`;
-            document.body.classList.add('finished-background'); // Adiciona a classe de fundo laranja quando o timer finalizar
+            this.pauseTimer();
         }
-    } else if (seconds === 0) {
-        minutes--;
-        seconds = 59;
-    } else {
-        seconds--;
     }
-    updateTimerDisplay();
+
+    startTimer() {
+        this.timer = setInterval(() => this.updateTimer(), 1000);
+        this.isRunning = true;
+        this.startButton.innerHTML = '<i class="fas fa-pause"></i>';
+        this.stopButton.disabled = false;
+        document.body.classList.remove('paused-background');
+    }
+
+    pauseTimer() {
+        clearInterval(this.timer);
+        this.isRunning = false;
+        this.startButton.innerHTML = '<i class="fas fa-play"></i>';
+        document.body.classList.add('paused-background');
+    }
+
+    stopTimer() {
+        clearInterval(this.timer);
+        this.isRunning = false;
+        this.minutes = this.studyTime;
+        this.seconds = 0;
+        this.isBreak = false;
+        this.updateTimerDisplay();
+        this.startButton.innerHTML = '<i class="fas fa-play"></i>'; // Atualiza o ícone do botão para start
+        this.stopButton.disabled = true;
+    }
     
-    // Notificação sonora quando o timer zerar
-    if (minutes === 0 && seconds === 0) {
-        playSound('beep.mp3');
+    resetTimer() {
+        this.stopTimer();
+        this.cyclesCompleted = 0;
+        this.cyclesDisplay.textContent = `Ciclos completos: ${this.cyclesCompleted}`;
+        document.body.classList.remove('finished-background');
+        this.startButton.innerHTML = '<i class="fas fa-play"></i>'; // Atualiza o ícone do botão para start
+    }
+    
+
+    updateSettings() {
+        this.studyTime = parseInt(this.studyTimeInput.value);
+        this.breakTime = parseInt(this.breakTimeInput.value);
+        this.studyValue.textContent = this.studyTime;
+        this.breakValue.textContent = this.breakTime;
+        if (!this.isRunning) {
+            this.minutes = this.studyTime;
+            this.seconds = 0;
+            this.updateTimerDisplay();
+        }
+    }
+
+    updateTimer() {
+        if (this.minutes === 0 && this.seconds === 0) {
+            if (this.isBreak) {
+                this.minutes = this.studyTime;
+                this.isBreak = false;
+                document.body.classList.remove('finished-background');
+            } else {
+                this.minutes = this.breakTime;
+                this.isBreak = true;
+                this.cyclesCompleted++;
+                this.cyclesDisplay.textContent = `Ciclos completos: ${this.cyclesCompleted}`;
+                document.body.classList.add('finished-background');
+            }
+            this.playSound('beep.mp3');
+        } else if (this.seconds === 0) {
+            this.minutes--;
+            this.seconds = 59;
+        } else {
+            this.seconds--;
+        }
+        this.updateTimerDisplay();
+    }
+    
+
+    playSound(soundFile) {
+        const audio = new Audio(soundFile);
+        audio.play();
+    }
+       
+    
+
+    updateTimerDisplay() {
+        const minutesDisplay = this.formatTime(this.minutes);
+        const secondsDisplay = this.formatTime(this.seconds);
+        this.timerDisplay.textContent = `${minutesDisplay}:${secondsDisplay}`;
+    }
+
+    formatTime(time) {
+        return time < 10? `0${time}` : time;
+    }
+
+    playSound(soundFile) {
+        const audio = new Audio(soundFile);
+        audio.play();
     }
 }
 
-// Função para notificação sonora
-function playSound(soundFile) {
-    const audio = new Audio(soundFile);
-    audio.play();
-}
-
-// Função para atualizar a exibição do temporizador
-function updateTimerDisplay() {
-    const minutesDisplay = minutes < 10 ? `0${minutes}` : minutes;
-    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
-    timerDisplay.textContent = `${minutesDisplay}:${secondsDisplay}`;
-    // Atualizar o anel de progresso
-    const circle = document.querySelector('.progress-ring-circle-fg');
-    const radius = circle.r.baseVal.value;
-    const circumference = radius * 2 * Math.PI;
-    const progress = (studyTime * 60 - (minutes * 60 + seconds)) / (studyTime * 60);
-    circle.style.strokeDasharray = `${circumference}`;
-    circle.style.strokeDashoffset = `${circumference * (1 - progress)}`;
-}
-
-// Função para iniciar o temporizador
-function startTimer() {
-    timer = setInterval(updateTimer, 1000);
-    isRunning = true;
-    startButton.innerHTML = '<i class="fas fa-pause"></i>';
-    stopButton.disabled = false;
-    document.body.classList.remove('paused-background'); // Remove a classe de fundo laranja
-}
-
-// Função para pausar o temporizador
-function pauseTimer() {
-    clearInterval(timer);
-    isRunning = false;
-    startButton.innerHTML = '<i class="fas fa-play"></i>';
-    document.body.classList.add('paused-background'); // Adiciona a classe de fundo laranja
-}
-
-// Função para parar o temporizador
-function stopTimer() {
-    clearInterval(timer);
-    isRunning = false;
-    minutes = studyTime;
-    seconds = 0;
-    isBreak = false;
-    updateTimerDisplay();
-    startButton.innerHTML = '<i class="fas fa-play"></i>';
-    stopButton.disabled = true;
-}
-
-// Função para resetar o temporizador
-function resetTimer() {
-    stopTimer();
-    cyclesCompleted = 0;
-    cyclesDisplay.textContent = `Ciclos completos: ${cyclesCompleted}`;
-    document.body.classList.remove('finished-background'); // Remove a classe de fundo laranja
-}
-
-// Função para atualizar os tempos de estudo e pausa
-function updateSettings() {
-    studyTime = parseInt(studyTimeInput.value);
-    breakTime = parseInt(breakTimeInput.value);
-    studyValue.textContent = studyTime;
-    breakValue.textContent = breakTime;
-    if (!isRunning) {
-        minutes = studyTime;
-        seconds = 0;
-        updateTimerDisplay();
-    }
-}
-
-// Adicionando eventos aos botões e controles deslizantes
-startButton.addEventListener('click', () => {
-    if (!isRunning) {
-        startTimer();
-    } else {
-        pauseTimer();
-    }
-});
-stopButton.addEventListener('click', stopTimer);
-resetButton.addEventListener('click', resetTimer);
-studyTimeInput.addEventListener('input', updateSettings);
-breakTimeInput.addEventListener('input', updateSettings);
-
-// Atualiza a exibição do temporizador com os valores iniciais
-updateTimerDisplay();
+const braila = new BrailaTimer();

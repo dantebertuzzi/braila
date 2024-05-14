7a00b8f4ef8efea88ff29dc3f0976dfe2be6b276
Dante Bertuzzi <67659136+dantebertuzzi@users.noreply.github.com>
2024-05-14 19:28:51 -0300

    Update timerWorker.js
    logica certa

--- timerWorker.js
+++ timerWorker.js
@@ -1,54 +1,59 @@
+let intervalId;
 let minutes;
 let seconds;
-let isRunning = false;
+let studyTime;
+let breakTime;
+let isBreak = false;
 
 self.onmessage = function(e) {
     const data = e.data;
-    switch (data.command) {
+    switch(data.command) {
         case 'start':
-            if (!isRunning) {
-                isRunning = true;
-                minutes = data.minutes;
-                seconds = data.seconds;
-                timer();
-            }
+            startTimer(data.minutes, data.seconds);
             break;
         case 'pause':
-            isRunning = false;
+            clearInterval(intervalId);
             break;
         case 'stop':
-            isRunning = false;
-            minutes = data.studyTime;
-            seconds = 0;
-            postMessage({ type: 'update', minutes, seconds });
-            break;
-        case 'reset':
-            isRunning = false;
-            minutes = data.studyTime;
-            seconds = 0;
-            postMessage({ type: 'update', minutes, seconds });
+            clearInterval(intervalId);
+            isBreak = false;
+            self.postMessage({ type: 'update', minutes: data.studyTime, seconds: 0 });
             break;
         case 'updateSettings':
-            minutes = data.studyTime;
-            seconds = 0;
-            postMessage({ type: 'update', minutes, seconds });
+            studyTime = data.studyTime;
+            breakTime = data.breakTime;
+            if (!isBreak) {
+                minutes = studyTime;
+                seconds = 0;
+            }
             break;
     }
 };
 
-function timer() {
-    if (!isRunning) return;
-    
-    if (minutes === 0 && seconds === 0) {
-        postMessage({ type: 'cycleComplete' });
-    } else if (seconds === 0) {
-        minutes--;
-        seconds = 59;
-    } else {
-        seconds--;
-    }
+function startTimer(startMinutes, startSeconds) {
+    minutes = startMinutes;
+    seconds = startSeconds;
 
-    postMessage({ type: 'update', minutes, seconds });
-    
-    setTimeout(timer, 1000);
+    intervalId = setInterval(() => {
+        if (seconds === 0) {
+            if (minutes === 0) {
+                clearInterval(intervalId);
+                self.postMessage({ type: 'cycleComplete' });
+                isBreak = !isBreak;
+                if (isBreak) {
+                    minutes = breakTime;
+                } else {
+                    minutes = studyTime;
+                }
+                seconds = 0;
+                startTimer(minutes, seconds);
+            } else {
+                minutes--;
+                seconds = 59;
+            }
+        } else {
+            seconds--;
+        }
+        self.postMessage({ type: 'update', minutes: minutes, seconds: seconds });
+    }, 1000);
 }
